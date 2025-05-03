e557c940ceeb22bb1e6da11ccbba8bb5e75ae219
Rewan Adel <96265779+Rewan-Adel@users.noreply.github.com>
2025-05-03 22:33:30 +0300

    Update UserService.js

--- Frontend/src/services/UserService.js
+++ Frontend/src/services/UserService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = 'https://todo-app-git-main-rewan-adels-projects.vercel.app/api';
+const API_URL = 'http://localhost:8000/api';
 
 export const getUser = async (token) => {
     try {
